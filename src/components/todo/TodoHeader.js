ee65508eca4e8b9f2836ba86b0e8e3954a4587d8
lgy <ini03134@naver.com>
2024-05-22 11:18:51 +0900

    회원 등급 승격 화면 처리

--- src/components/todo/TodoHeader.js
+++ src/components/todo/TodoHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../scss/TodoHeader.scss';
 
-const TodoHeader = ({ count }) => {
+const TodoHeader = ({ count, promote }) => {
   const today = new Date();
 
   const dateString = today.toLocaleDateString('ko-KR', {
@@ -13,6 +13,45 @@ const TodoHeader = ({ count }) => {
   const dayName = today.toLocaleDateString('ko-KR', {
     weekday: 'long', // 요일까지나오게
   });
+
+  const upgrade = () => {
+    if (
+      window.confirm(
+        '프리미엄으로 업그레이드 하시겠습니까?',
+      )
+    ) {
+      promote();
+    }
+  };
+
+  // 회원 등급에 따른 조건별 렌더링
+  const gradeView = () => {
+    const role = localStorage.getItem('USER_ROLE');
+    console.log('TodoHeader -> role: ', role);
+
+    if (role === 'COMMON') {
+      return (
+        <span
+          className='promote badge bg-warning'
+          onClick={upgrade}
+        >
+          일반회원
+        </span>
+      );
+    } else if (role === 'PREMIUM') {
+      return (
+        <span className='promote badge bg-danger'>
+          프리미엄회원
+        </span>
+      );
+    } else if (role === 'ADMIN') {
+      return (
+        <span className='promote badge bg-info'>
+          관리자
+        </span>
+      );
+    }
+  };
 
   return (
     <header>
@@ -27,6 +66,7 @@ const TodoHeader = ({ count }) => {
           <span>할 일 {count()}개 남음</span>
         )}
       </div>
+      {gradeView()}
     </header>
   );
 };
